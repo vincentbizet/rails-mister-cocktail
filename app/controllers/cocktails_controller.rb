847d227e374793daaa70ca4accdfc50c3393fd0e
vincentbizet <bizetvincent78@gmail.com>
2020-05-16 12:38:17 +0200

    controller doses et ingredients done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,21 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktails.new(params[:cocktail])
+    @cocktail.save
+  end
+
+  def home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'cocktails', to: 'cocktails#index'
-  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
-  get 'cocktails/new', to: 'cocktails#new'
+  root to: 'cocktails#home'
+  resources :cocktails, only: [:index, :show, :new, :create]
 end
