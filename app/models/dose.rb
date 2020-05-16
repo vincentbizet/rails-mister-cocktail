240c425e7826696e5112275ed3e800f3f0f7b1c0
vincentbizet <bizetvincent78@gmail.com>
2020-05-16 11:59:17 +0200

    models and validations OK

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates :ingredient_id, uniqueness: { scope: :cocktail_id }
+end
