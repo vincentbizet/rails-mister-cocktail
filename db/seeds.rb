8ecb32491ae5b1f9149ba31a0613b270a188c03c
vincentbizet <bizetvincent78@gmail.com>
2020-05-16 16:24:18 +0200

    fin samedi 16mai

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Ingredient.destroy_all
 
-Ingredient.create(name: "Amaretto")
-Ingredient.create(name: "Coffee brandy")
-Ingredient.create(name: "Firewater")
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
